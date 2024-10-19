47def079adce4e1599009314a5f5e4cd5610fe72
gustavocadev <gustavocadev@gmail.com>
2024-10-19 16:09:14 -0500

    refactor: update middleware to use getUserId instead of getSession for userId retrieval

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,12 @@
 import { createMiddleware } from '@solidjs/start/middleware';
-import { getSession } from './api/session';
+import { getSession, getUserId } from './api/session';
 import { redirect } from '@solidjs/router';
 
 export default createMiddleware({
   onRequest: [
     async (event) => {
       const pathname = new URL(event.request.url).pathname;
-
-      const session = await getSession();
-      const userId = session.data.userId;
+      const userId = await getUserId();
 
       if (pathname === '/login' && userId) {
         return redirect('/', 302);
